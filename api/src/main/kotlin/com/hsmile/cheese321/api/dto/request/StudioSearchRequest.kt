fe09798e9558696c5cb85a2dc2c63583a60873ba
pop <popramel25@gmail.com>
2025-06-07 23:50:05 +0900

    remove: studio api 삭제

--- api/src/main/kotlin/com/hsmile/cheese321/api/dto/request/StudioSearchRequest.kt
+++
@@ -1,5 +0,0 @@
-package com.hsmile.cheese321.api.dto.request
-
-data class StudioSearchRequest(
-    val keyword: String? = null
-)
--- api/src/main/kotlin/com/hsmile/cheese321/api/dto/response/StudioResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.hsmile.cheese321.api.dto.response
-
-data class StudioResponse(
-    val id: String,
-    val name: String,
-    val address: String
-)
