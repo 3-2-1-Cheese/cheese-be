02f521b81caffdd6e8f0bf9ae4b431096f4040f3
popop25 <popramel25@gmail.com>
2025-07-12 21:35:33 +0900

    feat: 사진 레포지트리 추가

---
+++ data/src/main/kotlin/com/hsmile/cheese321/data/photo/repository/PhotoRepository.kt
@@ -0,0 +1,32 @@
+package com.hsmile.cheese321.data.photo.repository
+
+import com.hsmile.cheese321.data.photo.entity.Photo
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+/**
+ * 사진 데이터 접근 Repository
+ */
+@Repository
+interface PhotoRepository : JpaRepository<Photo, String> {
+
+    /**
+     * 사용자별 사진 조회 (최신순)
+     */
+    fun findByUserIdOrderByCreatedAtDesc(userId: String): List<Photo>
+
+    /**
+     * 특정 사진관에서 찍은 사진들 조회
+     */
+    fun findByPhotoBoothIdOrderByCreatedAtDesc(photoBoothId: String): List<Photo>
+
+    /**
+     * 사용자별 특정 사진관 사진들 조회
+     */
+    fun findByUserIdAndPhotoBoothIdOrderByCreatedAtDesc(userId: String, photoBoothId: String): List<Photo>
+
+    // TODO: 나중에 구현할 기능들
+    // - 태그별 사진 검색
+    // - 파일 크기/타입별 필터링
+    // - 날짜 범위별 조회
+}
